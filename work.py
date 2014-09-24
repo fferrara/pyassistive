6e6fdbd076924bc5d2e2d933da7915a814832ce9
iena <thecrow.eric@gmail.com>
2014-09-24 17:01:16 -0300

    Implemented generator for getting data and dynamic plotting

--- work.py
+++ work.py
@@ -8,30 +8,58 @@ import matplotlib.pyplot as plt
 from collections import deque
 
 SERIAL = "SN201308221848GM" # only necessary for OSX
+PLOT_MIN_Y = -5
+PLOT_MAX_Y = 5
 
-def plot(y):
+def plot(y, yScale, bufferSize = 500):
     """
-        Greenlet that plot info once per millisecond
+        Greenlet that plot y once per .1
+        The y scale is specified through a tuple but is dynamically adjusted
     """
-    items = len(y)
-    buffer = deque([0]*items)
-    iterator = iter(y)
-    axes = plt.axes(xlim=(0, items), ylim=(min(y), max(y)))
-    line, = plt.plot(y)
+    buffer = deque([0]*bufferSize)
+    _ = plt.axes(xlim=(0, bufferSize), ylim=(0, 1000))
+    line, = plt.plot(buffer)
     plt.ion()
-
     plt.show()
-    # Foreach (x, y), insert at head the new item and remove the oldest
-    for i in xrange(0,items):
-        buffer.appendleft(next(iterator))
+
+    # Iterating the x axis, insert at head the new item and remove the oldest
+    for i in xrange(0,bufferSize):
+        # Adjusting Y scale
+        minY = min(buffer)
+        maxY = max(buffer)
+        plt.ylim([minY,maxY])
+        # Updating buffer
+        buffer.appendleft(next(y))
         buffer.pop()
+        # Plot refreshes with new buffer
         line.set_ydata(buffer)
         line.set_xdata(range(len(buffer)))
+
         plt.draw()
         print buffer[0]
-        time.sleep(0.001)
-        plt.pause(0.001)
+        gevent.sleep(0)
+        plt.pause(0.1)
 
+
+def getSensorsInfo(emotiv, sensorName, isQuality=False):
+    """
+        Greenlet to get a packet from Emotiv headset.
+        Return a generator with the value of the specified sensor.
+        If isQuality is True, value will represent the connection quality
+    """
+    try:
+        while True:
+            packet = emotiv.dequeue()
+            sensor = packet.sensors[sensorName]
+            value = sensor['quality'] if isQuality else sensor['value']
+            yield value
+            gevent.sleep(0)
+    except KeyboardInterrupt:
+        emotiv.close()
+    except GeneratorExit:
+        emotiv.close()
+    finally:
+        emotiv.close()
 
 if __name__ == "__main__":
     # Creating headset Object. No console output. No research headset
@@ -47,27 +75,7 @@ if __name__ == "__main__":
         quit()
 
     startTime = time.time()
-    tics = []
-    values = []
-    try:
-        while True:
-            # Obtain packet
-            tics.append(time.time() - startTime)
-            packet = headset.dequeue()
-            values.append(packet.gyro_x)
-            gevent.sleep(0)
-    except KeyboardInterrupt:
-        headset.close()
-    finally:
-        headset.close()
-
-    tics.pop(0)
-    values.pop(0)
-    #values = []
-
-
-    for i in xrange(len(tics)):
-        print "At {0:.10f}, value {1}".format(tics[i], values[i])
-
-    plot(values)
-
+    # Get the sensor data generator
+    data = getSensorsInfo(headset, "O1", False)
+    # Plot dynamically the value
+    plot(data, (PLOT_MIN_Y, PLOT_MAX_Y ) )
